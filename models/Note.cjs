388521507edce59881ed0bb070a4d75c21ad42ef
Fatimah <aishafhonore@gmail.com>
2023-09-28 13:13:46 -0500

    note Schema complete

---
+++ models/Note.cjs
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User", // Reference to the User model for associating notes with users
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Note", noteSchema);
